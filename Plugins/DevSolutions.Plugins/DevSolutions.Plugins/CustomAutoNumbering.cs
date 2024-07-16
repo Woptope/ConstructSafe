6b83860aa2ba248da5a652d101d3bd2d10d820ec
Woptope <52449718+Woptope@users.noreply.github.com>
2024-07-17 00:06:57 +0200

    Add files via upload

---
+++ Plugins/DevSolutions.Plugins/DevSolutions.Plugins/CustomAutoNumbering.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevSolutions.Plugins
+{
+    public class CustomAutoNumbering : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            ITracingService tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
+            {
+                Entity entity = (Entity)context.InputParameters["Target"];
+                IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+                IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+                try
+                {
+
+                    if (context.MessageName == "Create")
+                    {
+                        LockTransaction(service, tracingService);
+                        int nextNumber = GetNextAutoNumber(service, tracingService);
+                        entity["ds_nonconformitynumber"] = nextNumber.ToString();
+                    }
+                }
+                catch (FaultException<OrganizationServiceFault> ex)
+                {
+                    throw new InvalidPluginExecutionException("An error occurred in CustomNumberPlugin.", ex);
+                }
+                catch (Exception ex)
+                {
+                    tracingService.Trace("CustomAutoNumbering: {0}", ex.ToString());
+                    throw;
+                }
+            }
+        }
+
+        private void LockTransaction(IOrganizationService service, ITracingService tracingService)
+        {
+            tracingService.Trace("Locking transaction");
+
+            var qExpression = new QueryExpression("ds_nonconformitymanagementsetting");
+            qExpression.ColumnSet = new ColumnSet("ds_name");
+            qExpression.Criteria.AddCondition("ds_name", ConditionOperator.Equal, "Locker For Autonumbering");
+
+            var results = service.RetrieveMultiple(qExpression);
+            tracingService.Trace("Count of ds_nonconformitymanagementsetting records - {0}", results.Entities.Count);
+
+            var counter = results.Entities.First();
+            var blocker = new Entity("ds_nonconformitymanagementsetting") { Id = counter.Id };
+            blocker["ds_name"] = "Locker For Autonumbering";
+
+            service.Update(blocker); // Lock all transactions
+
+            tracingService.Trace("Transaction locked");
+        }
+
+        private int GetNextAutoNumber(IOrganizationService service, ITracingService tracingService)
+        {
+            tracingService.Trace("Retrieving next auto number");
+
+            var query = new QueryExpression("ds_nonconformity")
+            {
+                ColumnSet = new ColumnSet("ds_nonconformitynumber", "ds_nonconformityid")
+            };
+
+            query.Criteria.AddCondition("ds_nonconformitynumber", ConditionOperator.NotNull);
+            var retrievedRecords = service.RetrieveMultiple(query).Entities;
+            tracingService.Trace("Number of records retrieved: {0}", retrievedRecords.Count);
+
+            List<int> numbers = new List<int>();
+            foreach (Entity rec in retrievedRecords)
+            {
+                string nonconformityNumber = rec.Attributes["ds_nonconformitynumber"].ToString();
+                if (nonconformityNumber.All(char.IsDigit))
+                {
+                    numbers.Add(Convert.ToInt32(nonconformityNumber));
+                }
+            }
+
+            if (numbers.Count > 0)
+            {
+                int maxNumber = numbers.Max();
+                tracingService.Trace("Max number found: {0}", maxNumber);
+                return maxNumber + 1;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+    }
+}
